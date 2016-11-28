ea2e270a8d81b11f000657a0c23be7ddc330430e
Jonay Trenous <jtrenous@gmail.com>
2016-11-28 14:31:04 -0500

    Message Passing Method Headers

---
+++ code/messagepassing.py
@@ -0,0 +1,31 @@
+""" Implemementation of the message passing algorithm for planted clique.
+(Deshpande 2014)
+"""
+
+
+def msg_pass(W, d, t, rho):
+    """ msg pass.
+"""
+    return None
+
+
+def get_poly(d, t, mu):
+    """ Returns the sequence of polynomials p(l, z)
+    """
+
+
+
+def get_moment(a, n):
+    """ Returns E[Z + a]^n, Z~N(0,1)
+    """
+
+
+
+def get_mu(mu_t, L_t, d):
+    """ Returns mu_{t+1}
+    """
+
+
+def get_L(mu_t, d):
+    """ Returns L_t.
+    """
