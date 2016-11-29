a5bf3905247908f27e9a5d3a9e1348371ce13743
Ningshan Zhang <nzhang@Ningshans-MacBook-Pro.local>
2016-11-28 20:26:35 -0500

    put things together

--- code/messagepassing.py
+++ code/messagepassing.py
@@ -7,10 +7,16 @@ from scipy.misc import factorial2 as fac2
 from scipy.misc import factorial as fac
 from  scipy.special import binom
 import math
+import sample_graph
 
 def msg_pass(W, d, t, rho):
     """ msg pass.
-"""
+    """
+    poly = get_poly(d,t)
+    N = W.shape[1]
+    A = W/math.sqrt(N)
+
+    return belief_prop(A,t,poly)
 
 
 
@@ -91,3 +97,81 @@ def get_poly(d, t):
         return np.sum(np.multiply(coeffs, polys))
 
     return poly, L_list, mu_list
+
+
+def belief_prop_iter(A,theta_N, theta_NN, poly,t):
+    ''' Return theta_N and theta_NN for next iteration.
+    
+    Args:
+        A: W/sqrt(N), symmetric
+        theta_N: vector of length N
+        theta_NN: NxN matirx, assymetric. theta_NN[i,j] = msg i -> j
+        poly: a polynomial function 
+        t: time step, therefore f = poly(.,t)
+
+        
+    Returns:
+        theta_N: the new value for next iteration
+        theta_NN: the new value for next iteration
+     '''
+        
+    # number of nodes
+    N = A.shape[1]
+
+    # f(theta_{l->i})
+    F = poly(theta_NN,t)
+    F[np.diag_indices(N)] = 0
+
+    # compute theta_N 
+    # theta_N[i] = sum_{j!=i} A[j,i] * F[j,i]
+    #            = sum( A[,i] * F[,i]),
+    # since A[i,i] = F[i,i] = 0
+    theta_N = np.sum(F * A,0)
+
+    # compute theta_NN
+    # theta_NN[i,] = theta_N[i] - A[,i] * F[,i] 
+    theta_NN = np.reshape(np.repeat(theta_N,N),(N,N)) -A * F.T 
+   
+    return theta_N, theta_NN
+
+def belief_prop(A,T, poly):
+
+    ''' Run belief propagation and return theta_N, theta_NN .
+
+    Args:
+        A: W/sqrt(N)
+        T: number of rounds, t*
+        poly: a polynomial function
+
+    Returns:
+        theta_N
+        theta_NN
+    '''
+
+    # find number of nodes N
+    N = A.shape[1]
+
+    # let A have zero diagonal
+    A[np.diag_indices(N)] = 0
+
+    # initialize
+    theta_N = np.ones(N)
+    theta_NN = np.ones((N,N))
+    theta_NN[np.diag_indices(N)] = 0
+
+    # run T iterations
+    for t in np.arange(T):
+        theta_N,theta_NN = belief_prop_iter(A, theta_N,theta_NN, poly, t)
+
+    return  theta_N, theta_NN
+
+clique = np.array([1,1,1,1,0,0,0,0,0,0])
+W = sample_graph.sample_graph(10,0.5,clique)
+d = 2
+t = 10
+rho =1
+a,b= msg_pass(W,d,t,rho)
+print(a)
+print(b)
+
+
